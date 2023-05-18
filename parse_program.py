8b5c3ec59c5fd7fb55731c40fac958a2a67fabde
EnjoyGray <vanya.konrad@gmail.com>
2023-05-18 12:26:13 +0200

    update

--- parse_program.py
+++ parse_program.py
@@ -6,16 +6,17 @@ from bs4 import BeautifulSoup
 from time import sleep
 from datetime import datetime
 
-
+# Get the current date and time
 yy = datetime.now().year
 mm = datetime.now().month
 dd = datetime.now().day
 mtime = datetime.now().strftime("%H.%M")
 date = f"{yy}.{mm}.{dd}_{mtime}"
 
-
+# Function to retrieve information
 def pob_info_to():
     def pobieranie_id_z_to(link_strony):
+        # Retrieve IDs from the website
         req = requests.get(link_strony, headers=headers)
         src = req.text
         soup = BeautifulSoup(src, "lxml")
@@ -31,13 +32,14 @@ def pob_info_to():
     data_id = []
 
     for i in range(1, 7):
+        # Get IDs from "tabelaofert.pl" website
         pobieranie_id_z_to(fr"https://tabelaofert.pl/nowe-mieszkania?inwestycja_3d=1&page={i}")
 
     link_to = "https://tabelaofert.pl/nowe-ogrody-8.0-janusza-meissnera-3-poznan-jezyce-wola-mieszkania-na-sprzedaz,"
     project_data_list = []
     start = time.time()
-    for num in data_id:
 
+    for num in data_id:
         sleep(3)
         url = link_to + num
 
@@ -47,49 +49,48 @@ def pob_info_to():
 
         all_product = soup.find_all("div", class_="ng-sz-content inwestycja")
 
-        # Finding a file element individually
+        # Find project information individually
         for item in all_product:
             try:
-                inwest_name = item.find(class_="szczegoly-header__title").find("h1")  # nazwa inwestycji
+                inwest_name = item.find(class_="szczegoly-header__title").find("h1")  # investment name
             except Exception:
-                inwest_name = "Nie znaleziono"
+                inwest_name = "Not found"
 
             try:
-                devop_name = item.find(class_="szczegoly-header__title").find("h2")  # nazwa inswetora
+                devop_name = item.find(class_="szczegoly-header__title").find("h2")  # developer name
             except Exception:
-                devop_name = "Nie znaleziono"
+                devop_name = "Not found"
 
             try:
-                url_inwestora = item.find(class_="oferent-www").find("a").get("href")
+                url_inwestora = item.find(class_="oferent-www").find("a").get("href")  # developer website URL
             except Exception:
-                url_inwestora = "Nie znaleziono"
+                url_inwestora = "Not found"
 
             try:
-                makiet = item.find("div", class_="gallery-static__item-ico")  # czy jest makiet 3d
+                makiet = item.find("div", class_="gallery-static__item-ico")  # presence of 3D model
             except Exception:
-                makiet = "Nie znaleziono"
+                makiet = "Not found"
 
             try:
                 plan_kondygnacji = item.find("div", class_="gallery-static__thumbs-plan-kondygnacji")\
-                   .get("data-popup-tab")  # czy sa plany kontygnacij
+                   .get("data-popup-tab")  # presence of floor plans
             except Exception:
-                plan_kondygnacji = "Nie znaleziono"
+                plan_kondygnacji = "Not found"
 
             try:
-                # Czy 1 wizka jest obrysowana
                 wizki = item.find_next(class_="gallery-static__images-container").find("svg")
                 wizki = str(wizki)
                 if "svg" in wizki:
-                    wizki = "Wizka_Obrysowana"
+                    wizki = "Visualization_Outlined"
                 else:
-                    wizki = "jest do zrobienia"
+                    wizki = "To be done"
             except Exception:
-                wizki = "Nie znaleziono"
+                wizki = "Not found"
 
             try:
-                ilosc_wizek = item.find(class_="gallery-static__image-count")  # ilosc wizek
+                ilosc_wizek = item.find(class_="gallery-static__image-count")  # number of visualizations
             except Exception:
-                ilosc_wizek = "Nie znaleziono"
+                ilosc_wizek = "Not found"
 
             project_data_list.append(
                 {
@@ -114,7 +115,7 @@ def pob_info_to():
         print(df)
     end = time.time()
     total = end - start
-    print("Пройшло часу: ", "%.2f" % total)
+    print("Elapsed time: ", "%.2f" % total)
 
 
 if __name__ == '__main__':
